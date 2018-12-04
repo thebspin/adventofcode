9e78b4a5939c0185e70cf2cf0fc14e582014b9ad
Dennis K <d.keirs@gmail.com>
2018-12-04 15:13:30 +0100

    Updated chal2 day 1

--- 2018/day1/chal2.py
+++ 2018/day1/chal2.py
@@ -1,14 +1,17 @@
 # The file to read
-#filepath = 'chal1input.txt'
-filepath = 'testinput.txt'
+filepath = 'chal1input.txt'
+#filepath = 'testinput.txt'
 
 stop = False
 checkList = list()
+result = 0
+
+checkList.append(result) # Dont forget to add 0 the first time
 
 def loopList():
     with open(filepath) as fp:
-        result = 0
-        checkList.append(result) # Dont forget to add 0 the first time to
+        global result
+        global stop
     
         for line in fp:
             result += int(float(line))
@@ -18,11 +21,15 @@ def loopList():
                 stop = True
                 break
         
-            checkList.append(result)
-       
-        print("Number of items", len(checkList))
+            checkList.append(result) 
+
+runs = 0
 
 while True:
+    print("Run number: ", runs)
     loopList()
     if (stop):
         break
+    runs+=1
+    
+print("Number of items", len(checkList))
